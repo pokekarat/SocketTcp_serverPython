1e133d6f79300502f84c3fcbf5d61c20fc974988
Freddy Rodrigo Mendoza Ticona <freddy12120@gmail.com>
2016-06-06 22:54:45 -0500

    add python code

---
+++ socketcp_gsm_py.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+import socket
+import sys
+import os, time
+
+
+os.environ['TZ'] = 'America/Lima'
+time.tzset()
+
+
+
+TCP_IP = '172.31.47.57'
+TCP_PORT = 50000
+BUFFER_SIZE = 20
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.bind((TCP_IP, TCP_PORT))
+s.listen(1)
+
+while 1:
+	conn, addr = s.accept()
+	print 'Connection address:', addr
+	hora = time.strftime('%X')
+ 	year = time.strftime('%Y')
+	month = time.strftime('%m')
+	day = time.strftime('%d')
+
+	datafile = open('Dataset_'+hora+'_'+day+':'+month+':'+year+'.dat','w')
+
+	while 1:
+    	data = conn.recv(BUFFER_SIZE)
+    	if not data: break
+    	print "received data:", data
+    	conn.send(data)  # echo
+    	datafile.write(data+'\n')
+datafile.close()
+conn.close()
