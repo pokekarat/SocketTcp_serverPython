40e4f69ad378c9688803c5496de060a5ce07d594
Freddy Rodrigo Mendoza Ticona <freddy12120@gmail.com>
2017-05-18 15:32:06 -0500

    change python code

--- socketcp_gsm_py.py
+++ socketcp_gsm_py.py
@@ -7,7 +7,7 @@ import os, time
 os.environ['TZ'] = 'America/Lima'
 time.tzset()
 
-TCP_IP = '172.31.47.57'
+TCP_IP = '172.31.46.42'
 TCP_PORT = 50000
 BUFFER_SIZE = 20
 
@@ -16,20 +16,21 @@ s.bind((TCP_IP, TCP_PORT))
 s.listen(1)
 
 while 1:
-	conn, addr = s.accept()
-	print 'Connection address:', addr
-	hora = time.strftime('%X')
- 	year = time.strftime('%Y')
-	month = time.strftime('%m')
-	day = time.strftime('%d')
+        conn, addr = s.accept()
+        print 'Connection address:', addr
+        hora = time.strftime('%X')
+        year = time.strftime('%Y')
+        month = time.strftime('%m')
+        day = time.strftime('%d')
 
-	datafile = open('Dataset_'+hora+'_'+day+':'+month+':'+year+'.dat','w')
+        datafile = open('/var/www/html/DATAserver/Dataset_'+hora+'_'+day+':'+month+':'+year+'.dat','w')
 
-	while 1:
-    	data = conn.recv(BUFFER_SIZE)
-    	if not data: break
-    	print "received data:", data
-    	conn.send(data)  # echo
-    	datafile.write(data+'\n')
-datafile.close()
+        while 1:
+                data = conn.recv(BUFFER_SIZE)
+                if not data: break
+                print "received data:", data
+                conn.send(data)  # echo
+                datafile.write(data+'\n')
+        datafile.close()
 conn.close()
+
